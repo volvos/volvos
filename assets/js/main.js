864cc4611bce14c0948aa2053b79f8cd09bcf29f
Volkan Dayanc <volvosum@gmil.com>
2022-04-12 02:16:20 +0300

    Class, Lesson, Section Choose ok.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,40 +1,149 @@
 function addQuestion() {
+    var konuID=document.getElementById('konuID').value
+    var isImage="0"
     var question = document.getElementById('questionText').value
     if (question=="") {
-        question= document.getElementById('questionImg').style.backgroundImage
+        question=document.getElementById('questionImg').style.backgroundImage
+        question= question.replace(`url("data:image/png;base64,`,"")
+        question= question.replaceAll("+","€")
+        question= question.replace(`")`,`"`)
+        isImage="1"
     }
     var answer1 = document.getElementById('answerText1').value
     if (answer1=="") {
-        answer1= document.getElementById('answerImg1').style.backgroundImage
+        answer1=document.getElementById('answerImg1').style.backgroundImage
+        answer1= answer1.replace(`url("data:image/png;base64,`,"")
+        answer1= answer1.replaceAll("+","€")
+        answer1= answer1.replace(`")`,`"`)
     }
     var answer2 = document.getElementById('answerText2').value
     if (answer2=="") {
-        answer2= document.getElementById('answerImg2').style.backgroundImage
+        answer2=document.getElementById('answerImg2').style.backgroundImage
+        answer2= answer2.replace(`url("data:image/png;base64,`,"")
+        answer2= answer2.replaceAll("+","€")
+        answer2= answer2.replace(`")`,`"`)
     }
     var answer3 = document.getElementById('answerText3').value
     if (answer3=="") {
-        answer3= document.getElementById('answerImg3').style.backgroundImage
+        answer3=document.getElementById('answerImg3').style.backgroundImage
+        answer3= answer3.replace(`url("data:image/png;base64,`,"")
+        answer3= answer3.replaceAll("+","€")
+        answer3= answer3.replace(`")`,`"`)
     }
     var answer4 = document.getElementById('answerText4').value
     if (answer4=="") {
-        answer4= document.getElementById('answerImg4').style.backgroundImage
+        answer4=document.getElementById('answerImg4').style.backgroundImage
+        answer4= answer4.replace(`url("data:image/png;base64,`,"")
+        answer4= answer4.replaceAll("+","€")
+        answer4= answer4.replace(`")`,`"`)
     }
     var answer5 = document.getElementById('answerText5').value
     if (answer5=="") {
-        answer5= document.getElementById('answerImg5').style.backgroundImage
+        answer5=document.getElementById('answerImg5').style.backgroundImage
+        answer5= answer5.replace(`url("data:image/png;base64,`,"")
+        answer5= answer5.replaceAll("+","€")
+        answer5= answer5.replace(`")`,`"`)
     }
     var answerCorrect = document.getElementById('answerCorrect').value
 
-    console.log(question)
-    console.log(answer1)
-    console.log(answer2)
-    console.log(answer3)
-    console.log(answer4)
-    console.log(answer5)
-    console.log(answerCorrect)
+    var result="";
+    $.ajax({
+        url:'/add_question?l1=' + question +"&l2=" +answer1+"&l3=" + answer2+"&l4=" + answer3+"&l5=" + answer4+"&l6=" + answer5+"&l7=" + answerCorrect+"&l8=" + isImage +"&k="+konuID,
+        async: false,  
+        success:function(data) {
+            result = data.value; 
+            if (result=1) {
+                console.log("Kayıt ok")
+                if (document.getElementById('questionText').value=="") {
+                    document.getElementById('questionImg').classList.remove("drop-zone__thumb")
+                }
+                if (document.getElementById('answerText1').value=="") {
+                    document.getElementById('answerImg1').classList.remove("drop-zone-1__thumb")
+                }
+                if (document.getElementById('answerText2').value=="") {
+                    document.getElementById('answerImg2').classList.remove("drop-zone-2__thumb")
+                }
+                if (document.getElementById('answerText3').value=="") {
+                    document.getElementById('answerImg3').classList.remove("drop-zone-3__thumb")
+                }
+                if (document.getElementById('answerText4').value=="") {
+                    document.getElementById('answerImg4').classList.remove("drop-zone-4__thumb")
+                }
+                if (document.getElementById('answerText5').value=="") {
+                    document.getElementById('answerImg5').classList.remove("drop-zone-5__thumb")
+                }
+                document.getElementById('questionText').value=""
+                document.getElementById('answerText1').value=""
+                document.getElementById('answerText2').value=""
+                document.getElementById('answerText3').value=""
+                document.getElementById('answerText4').value=""
+                document.getElementById('answerText5').value=""
+                document.getElementById('answerCorrect').value=""
+            }
+        }
+    });
 }
 
 function auto_height(elem) { 
     elem.style.height = "1px";
     elem.style.height = (elem.scrollHeight)+"px";
 }
+
+function classBind() {
+    $.ajax({
+        url:'firstBind',
+        async: false,  
+        success:function(data) {
+            if (data.Status==1) {
+                $("#sinifID").empty()
+                $("#sinifID").append(data.Value)
+            } else {
+                alert("Veri bulanamadı....")
+            }
+        }
+    })
+}
+function lessonBind(value) {
+    document.getElementById('dersID').classList.remove("section_hide")
+    $.ajax({
+        url:'secondBind?c=' + value,
+        async: false,  
+        success:function(data) {
+            if (data.Status==1) {
+                $("#dersID").empty()
+                $("#dersID").append(data.Value)
+            } else {
+                alert("Veri bulanamadı....")
+                document.getElementById('dersID').classList.add("section_hide")
+                document.getElementById('konuID').classList.add("section_hide")
+            }
+        }
+    })
+}
+function sectionBind(value) {
+    document.getElementById('konuID').classList.remove("section_hide")
+    $.ajax({
+        url:'thirdBind?s=' + value,
+        async: false,  
+        success:function(data) {
+            if (data.Status==1) {
+                $("#konuID").empty()
+                $("#konuID").append(data.Value)
+            } else {
+                alert("Veri bulanamadı....")
+                document.getElementById('konuID').classList.add("section_hide")
+            }
+        }
+    })
+}
+
+$(document).ready(function () {
+    classBind()
+});
+
+function questionInput(value) {
+    document.getElementById('addQuestion_Section').classList.remove("section_hide")
+}
+
+
+
--- main.go
+++ main.go
@@ -3,17 +3,28 @@ package main
 import (
 	"bufio"
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 
 	_ "github.com/go-sql-driver/mysql"
 )
+
+type returnAddDB struct {
+	Value int
+}
+type returnClass struct {
+	Value  string
+	Status int
+}
 
 var tpl *template.Template
 var db *sql.DB
@@ -32,6 +43,10 @@ func main() {
 	r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets/"))))
 
 	r.HandleFunc("/", indexHandler)
+	r.HandleFunc("/add_question", insertQuestion)
+	r.HandleFunc("/firstBind", classBind)
+	r.HandleFunc("/secondBind", lessonBind)
+	r.HandleFunc("/thirdBind", sectionBind)
 
 	srv := &http.Server{Handler: handlers.CompressHandler(r), Addr: ":8081"}
 	log.Fatal(srv.ListenAndServe())
@@ -60,8 +75,30 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func insertQuestion(w http.ResponseWriter, r *http.Request) {
+	getQuestion := r.URL.Query().Get("l1")
+	getQuestion = strings.Replace(getQuestion, "€", "+", -1)
+	getQuestion = strings.TrimRight(getQuestion, `"`)
+	getFirstAnswer := r.URL.Query().Get("l2")
+	getFirstAnswer = strings.Replace(getFirstAnswer, "€", "+", -1)
+	getFirstAnswer = strings.TrimRight(getFirstAnswer, `"`)
+	getSecondAnswer := r.URL.Query().Get("l3")
+	getSecondAnswer = strings.Replace(getSecondAnswer, "€", "+", -1)
+	getSecondAnswer = strings.TrimRight(getSecondAnswer, `"`)
+	getThirdAnswer := r.URL.Query().Get("l4")
+	getThirdAnswer = strings.Replace(getThirdAnswer, "€", "+", -1)
+	getThirdAnswer = strings.TrimRight(getThirdAnswer, `"`)
+	getFourthAnswer := r.URL.Query().Get("l5")
+	getFourthAnswer = strings.Replace(getFourthAnswer, "€", "+", -1)
+	getFourthAnswer = strings.TrimRight(getFourthAnswer, `"`)
+	getFifthAnswer := r.URL.Query().Get("l6")
+	getFifthAnswer = strings.Replace(getFifthAnswer, "€", "+", -1)
+	getFifthAnswer = strings.TrimRight(getFifthAnswer, `"`)
+	getCorrect := r.URL.Query().Get("l7")
+	getIsImage := r.URL.Query().Get("l8")
+	getKonuID := r.URL.Query().Get("k")
+
 	var insertUsers *sql.Stmt
-	insertUsers, err := db.Prepare("insert into math.turkishexam (section_id, question,first_answer, second_answer, third_answer, fourth_answer, fifth_answer, answer, difficulty, is_image) VALUES (?, ?, ?, ?, ?, ?, ?, ?,?,?)")
+	insertUsers, err := db.Prepare("insert into math.turkishexams (section_id, question,first_answer, second_answer, third_answer, fourth_answer, fifth_answer, answer, difficulty, is_image) VALUES (?, ?, ?, ?, ?, ?, ?, ?,?,?)")
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -69,5 +106,85 @@ func insertQuestion(w http.ResponseWriter, r *http.Request) {
 	defer insertUsers.Close()
 
 	var _ sql.Result
-	_, err = insertUsers.Exec("", "", "", "", "", "", "", "", "", "")
+	_, err = insertUsers.Exec(getKonuID, getQuestion, getFirstAnswer, getSecondAnswer, getThirdAnswer, getFourthAnswer, getFifthAnswer, getCorrect, 1, getIsImage)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	} else {
+		jsonfinal := returnAddDB{1}
+		js, _ := json.Marshal(jsonfinal)
+		w.Write(js)
+	}
+}
+
+func classBind(w http.ResponseWriter, r *http.Request) {
+	var classID, status int
+	var className string
+	strArray := ""
+
+	rows, err := db.Query("select id, name from class")
+	if err != nil {
+	} else {
+		for rows.Next() {
+			if err := rows.Scan(&classID, &className); err != nil {
+				status = 0
+			} else {
+				strArray += `<option value="` + strconv.Itoa(classID) + `">` + className + `</option>`
+				status = 1
+			}
+		}
+	}
+	w.Header().Set("Content-Type", "application/json")
+	jsonfinal := returnClass{strArray, status}
+	js, _ := json.Marshal(jsonfinal)
+	w.Write(js)
+}
+
+func lessonBind(w http.ResponseWriter, r *http.Request) {
+	getClassID := r.URL.Query().Get("c")
+	var classID, status int
+	var className string
+	strArray := ""
+
+	rows, err := db.Query("select id,name from lessons where class_id=? order by id", getClassID)
+	if err != nil {
+	} else {
+		for rows.Next() {
+			if err := rows.Scan(&classID, &className); err != nil {
+				status = 0
+			} else {
+				strArray += `<option value="` + strconv.Itoa(classID) + `">` + className + `</option>`
+				status = 1
+			}
+		}
+	}
+	w.Header().Set("Content-Type", "application/json")
+	jsonfinal := returnClass{strArray, status}
+	js, _ := json.Marshal(jsonfinal)
+	w.Write(js)
+}
+
+func sectionBind(w http.ResponseWriter, r *http.Request) {
+	getLessonID := r.URL.Query().Get("s")
+	var classID, status int
+	var className string
+	strArray := ""
+
+	rows, err := db.Query("select id,name from section where lesson_id=? order by id", getLessonID)
+	if err != nil {
+	} else {
+		for rows.Next() {
+			if err := rows.Scan(&classID, &className); err != nil {
+				status = 0
+			} else {
+				strArray += `<option value="` + strconv.Itoa(classID) + `">` + className + `</option>`
+				status = 1
+			}
+		}
+	}
+	w.Header().Set("Content-Type", "application/json")
+	jsonfinal := returnClass{strArray, status}
+	js, _ := json.Marshal(jsonfinal)
+	w.Write(js)
 }
